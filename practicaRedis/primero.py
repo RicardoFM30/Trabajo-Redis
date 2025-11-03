7881db8c4755ed73933ea0dce4615d0d8eb361ce
Ricardo Fernandez <ricardo.fernandez.guzman.al@iespoligonosur.org>
2025-11-03 21:02:28 +0100

    Segundo commit

---
+++ practicaRedis/primero.py
@@ -0,0 +1,21 @@
+#1 - Crear registros clave-valor(0.25 puntos)
+
+
+
+
+#2 - Obtener y mostrar el número de claves registradas (0.25 puntos)
+#3 - Obtener y mostrar un registro en base a una clave (0.25 puntos)
+#4 - Actualizar el valor de una clave y mostrar el nuevo valor(0.25 puntos)
+#5 - Eliminar una clave-valor y mostrar la clave y el valor eliminado(0.25 puntos)
+#6 - Obtener y mostrar todas las claves guardadas (0.25 puntos)
+#7 - Obtener y mostrar todos los valores guardados(0.25 puntos)
+#8 - Obtener y mostrar varios registros con una clave con un patrón en común usando * (0.5 puntos)
+#9 - Obtener y mostrar varios registros con una clave con un patrón en común usando [] (0.5 puntos)
+#10 - Obtener y mostrar varios registros con una clave con un patrón en común usando ? (0.5 puntos)
+#11 - Obtener y mostrar varios registros y filtrarlos por un valor en concreto. (0.5 puntos)
+#12 - Actualizar una serie de registros en base a un filtro (por ejemplo aumentar su valor en 1 )(0.5 puntos)
+#13 - Eliminar una serie de registros en base a un filtro (0.5 puntos)
+#14 - Crear una estructura en JSON de array de los datos que vayais a almacenar(0.5 puntos)
+#15 - Realizar un filtro por cada atributo de la estructura JSON anterior (0.5 puntos)
+#16 - Crear una lista en Redis (0.25 puntos)
+#17 - Obtener elementos de una lista con un filtro en concreto(0.5 puntos)
